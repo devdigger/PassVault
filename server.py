b7b59adf56872ad1106ea7164ee31e157f3b8a18
Sagar Kumar <aec.cse.sagarkumar@gmail.com>
2023-07-02 18:16:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,19 @@
 import os
-os.system("ls")
-os.system("python manage.py runserver 0.0.0.0:3000")
+import sys
+from django.core.management import execute_from_command_line
+from django.conf import settings
+
+# Set the Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'password_manager.settings')
+
+def start_django_server():
+    # Execute the Django server command with the desired port (e.g., 8000)
+    execute_from_command_line(['manage.py', 'runserver', '8000'])
+
+if __name__ == '__main__':
+    # Perform any necessary setup before starting the server
+    
+    # For example, you can add custom logic here
+    
+    # Start the Django server
+    start_django_server()
