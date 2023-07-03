a58e3783d0fe20128a775044cc165842a7c05b2f
Sagar Kumar <aec.cse.sagarkumar@gmail.com>
2023-07-03 09:01:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import sys
-os.system("pip install django cryptography bcrypt")
 from django.core.management import execute_from_command_line
 from django.conf import settings
 
